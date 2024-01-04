32dc271a0c85ebc496587d59c57e7a129cac5821
Anuj Rawat <anuj22rawat20@gmail.com>
2024-01-04 11:28:02 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,9 @@ app.use(express.json())
 app.use(cors({ origin: 'https://lms-backend-two.vercel.app', credentials: true }));
 app.use(cookieParser())
 
-app.use(async (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://lms-backend-two.vercel.app');
-    res.header('Access-Control-Allow-Credentials', true);
-})
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://lms-backend-two.vercel.app")
+  }) 
 
 app.use("/api/users", UserRouter)
 
